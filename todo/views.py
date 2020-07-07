afb21931d158b56764080796139bca927e709688
Samson <samsonadejoro@gmail.com>
2020-07-07 10:29:03 +0100

    Added delete todo feature

--- todo/views.py
+++ todo/views.py
@@ -12,8 +12,16 @@ def index(request):
             due_date = str(request.POST["due_date"]) #date
             category = request.POST["category_select"] #category
             Todo = Task(description = description, title=title, due_date=due_date, category=Category.objects.get(name=category))
+            print(Todo)
             Todo.save() #saving the todo 
             return redirect("/") #reloading the page
+
+        if "taskDelete" in request.POST: #checking if there is a request to delete a todo
+            checkedlist = request.POST.getlist('checkedbox') #checked todos to be deleted
+            for task_id in checkedlist:
+                todo = Task.objects.get(id=int(task_id)) #getting todo id
+                todo.delete()#deleting todo
+                return redirect("/") #reloading the page
     return render(request, "index.html", {'tasks':tasks, 'categories':categories})
 
 
